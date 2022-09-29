949a59d265d9454f15bc42aa792044ffb67ba924
Christopher Ng <facboy@gmail.com>
2022-09-29 11:00:46 +0100

    Fix my build script for linux kernel 5.15

--- myscripts/build_wsl_kernel.sh
+++ myscripts/build_wsl_kernel.sh
@@ -11,15 +11,16 @@ fi
 WSL2_DIR="/mnt/f/wsl/kernel/"
 
 echo "Building kernel"
-make deb-pkg
+make KCONFIG_CONFIG=Microsoft/config-wsl deb-pkg
 
 # work out kernel version
-KERNEL_VER="$(ls ../linux-*.buildinfo | gawk -- 'BEGIN { regex="^.*linux-([0-9.]+)-.*$" } $0 ~ regex { ver = gensub(regex, "\\1", 1); print ver }')"
+KERNEL_VER="$(ls -1tr ../linux-upstream_*.buildinfo | tail -n1 | gawk -- 'BEGIN { regex="^.*linux-upstream_([0-9.]+)-.*$" } $0 ~ regex { ver = gensub(regex, "\\1", 1); print ver }')"
 
 echo "Copying bzImage to ${WSL2_DIR}"
 cp "./arch/x86/boot/bzImage" "${WSL2_DIR}bzImage-${KERNEL_VER}-WSL2-mine"
 
-ARCHIVE_DIR="~/kernel/${KERNEL_VER}/"
+ARCHIVE_DIR="${HOME}/kernel/${KERNEL_VER}/"
 echo "Archiving build artifacts to ${ARCHIVE_DIR}"
 mkdir "${ARCHIVE_DIR}"
-cp ../linux-*${KERNEL_VER}* "${ARCHIVE_DIR}"
+cp ../linux-upstream_*${KERNEL_VER}* "${ARCHIVE_DIR}"
+cp ../linux-*${KERNEL_VER}*.deb "${ARCHIVE_DIR}"
