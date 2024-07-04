38747a13675cad245d11dc3966d506a255e7a149
Christopher Ng <facboy@gmail.com>
2024-07-04 18:39:11 +0100

    Fix `my_rebase.sh` for 6.6 workflow

--- myscripts/my_rebase.sh
+++ myscripts/my_rebase.sh
@@ -8,18 +8,18 @@ fi
 
 set -e
 
-MINE_BRANCH="mine-6.1"
-MINE_BASE="mine-6.1-base"
-UPSTREAM_BRANCH="linux-6.1.y"
-UPSTREAM="linux-stable/${UPSTREAM_BRANCH}"
-MY_UPSTREAM_BRANCH="linux-mine-wsl-6.1.y"
+MINE_BRANCH="mine-6.6"
+MINE_BASE="mine-6.6-base"
+UPSTREAM_BRANCH="linux-msft-wsl-6.6.y"
+UPSTREAM="upstream/${UPSTREAM_BRANCH}"
+MY_UPSTREAM_BRANCH="linux-mine-wsl-6.6.y"
 
 if [[ "$(git branch --show-current)" != "${MINE_BRANCH}" ]]; then
 	>&2 echo "Not on ${MINE_BRANCH} branch"
 	exit 1
 fi
 
-git fetch linux-stable --prune
+git fetch upstream --prune
 
 # check out my upstream branch and merge upstream
 git co "${MY_UPSTREAM_BRANCH}"
